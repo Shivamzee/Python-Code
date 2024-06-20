bfd0198ef8e9b741f74deaf92b1b41ca8a34d9a6
Shivamzee <hello.shivamzee@gmail.com>
2024-06-20 07:52:46 +0530

    file basic-questions pushed into w2-Python-Questions branch

---
+++ basic-questions.py
@@ -0,0 +1,19 @@
+# Q-1 Write a  Python program to print the following string in a specific format (see the output).
+#print("\"Learning gives creativity,\nCreativity leads to thinking,\n\t\tThinking provides knowledge,\n\t\tKnowledge makes you great.\"\n\t\t\t\t\t -DR. KALAM SIR")
+
+#--------------------------------------------------------------------------------------------------------------
+# Q-2 Write a  Python program to find out what version of  Python you are using.
+'''
+First we have to import 'sys' module -->
+Note : 'sys' module provides access to some variables used or maintained by the interpreter and to functions that interact strongly with the interpreter.
+'''
+import sys
+print("Python version:")
+
+# Use the sys.version attribute to get the Python version and print it
+print(sys.version)
+
+
+print("Version info:")
+# Use the sys.version_info attribute to get detailed version information and print it.
+print(sys.version_info)
