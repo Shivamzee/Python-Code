31c85b31b21a4be232e078755ebe4363d32af824
Shivamzee <hello.shivamzee@gmail.com>
2024-07-02 09:50:40 +0530

    string methods vid 13

---
+++ number_type.py
@@ -0,0 +1,17 @@
+"""
+#Number Data Type (int , float , complex)
+a = 28
+b = 231.32
+c = 3j
+print("Type of variable a =>" , type(a),"\n Type of variable b =>",type(b))
+print("Type of variable c =>" , type(c))
+
+"""
+
+# Q-1 Add Two Numbers With User Input.
+x = input("Enter first Number:")
+y = input("Enter second Number:")
+z = float(x) + float(y)
+print("The sum of " ,x ,",", y , "is :",z)
+
+
