a9a7bcdaf3bfe164aa52a2c2f489b87f88a6c920
Shivamzee <hello.shivamzee@gmail.com>
2024-07-02 09:50:19 +0530

    string methods

---
+++ string.py
@@ -0,0 +1,16 @@
+"""
+BankManager = 'Kartik Bharadwaj'
+print(BankManager)
+print(BankManager[0:6])
+print(len(BankManager))
+"""
+
+nm = "harry"
+print(nm[-4:-2])
+print(len(nm))
+
+# String methods tut 13 harry bhai
+
+myJob = 'Frontend Dev'
+print(myJob.upper())
+print(myJob.lower())
