cebb340a2157cccba123c5b88150ab31b232780f
Shivamzee <hello.shivamzee@gmail.com>
2024-07-20 17:54:36 +0530

    ffsf

--- ifelse.py
+++ ifelse.py
@@ -1,1 +1,55 @@
-print('It is day 14')
+import time
+
+# print('It is day 14')
+
+"""
+your_age = int(input('Enter your age: '))
+print('Your age is: ' , your_age)
+"""
+
+# Conditional operators
+"""
+print(your_age > 23)
+print(your_age < 33)
+print(your_age >= 30)
+print(your_age <= 55)
+print(your_age == 18)
+print(your_age != 24)
+
+if(your_age < 15):
+    print('Enjoy your life , just chill')
+else:
+    print(''' Learn and Grow , Don't Give Up  ''')
+
+
+ => nested if else 
+if(your_age < 15):
+    print('Enjoy your life , just chill')
+elif(your_age ==30):
+    print('spend time with your faimly')
+    
+elif(your_age >= 20):
+     print("Explore the fields , you can work")
+else:
+    print(''' Learn and Grow , Don't Give Up  ''')
+."""
+
+timestamp = time.strftime('%H:%M:%S')
+print(timestamp)
+
+
+# Greet Program in python
+greet_time = int(time.strftime('%H'))
+
+if(greet_time < 4):
+    print('Are o Nisachar ho rahi hai so jaao')
+elif(greet_time < 12):
+    print('Good Morning Sir ji..')
+elif(greet_time >=12 and greet_time<=16):
+    print('Good Noon')
+elif(greet_time >=16 and greet_time<=20 ):
+    print('Good Evening')
+elif(greet_time > 20 and greet_time < 24):
+    print('Good Night')
+else:
+    print('Sure , Tum Other Grah ke') 
---
+++ loops.py
@@ -0,0 +1,59 @@
+
+"""
+my_name = 'Shivam'
+for name in my_name:
+    print(name.upper())
+"""
+
+'''
+fruits_Name = ['Apple', 'grapes','Fig','orange','blackberry']
+for fruit in fruits_Name:
+    # print(fruit)
+    #print(fruit.capitalize())
+    if(fruit == 'Apple'):
+        for f in fruit:
+            print(f)
+'''
+
+
+'''
+# Print 1 to 10 numbers 
+for num in range(11):
+    print(num)
+
+# Print even number between 5 to 15
+for num in range(5 , 15):
+    #print(num)
+    if(num% 2 == 0):
+        print(num)      
+'''
+    
+'''
+# While loop in python
+count = int(input('Enter your Number: '))
+print(count)
+while (count < 20): 
+    print("You are Eligible for  next round")
+    if(count == count):
+        break
+    count = count + 1
+'''
+
+"""
+# Print table of 15 in python
+for table in range(12):
+    if(table == 10):
+       break
+    print('15 X ' , table+1 , ' = ' , 15*(table + 1))
+
+print('Table of 15 is completed')
+"""
+
+
+
+# Print the table of 23 
+for table23 in range(12):
+    if(table23 == 10):
+        break
+    print('23 X' , table23 + 1 , ' = ' , 13*(table23 + 1))
+    
