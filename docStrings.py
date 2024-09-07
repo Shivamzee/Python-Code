3e77e7dd660cfffdcaab27c54a594e0e4713c715
Shivamzee <hello.shivamzee@gmail.com>
2024-09-07 21:32:30 +0530

    learn recursion in python

---
+++ docStrings.py
@@ -0,0 +1,15 @@
+
+
+def person_quot(n):
+    '''
+    Here are only kalam sir's quots.
+    '''
+    print( n,"Change yourself as soon as possible.")
+
+
+person_quot("Shivam ")
+print(person_quot.__doc__)
+
+
+for i in range(10):
+    print(i)
---
+++ fstring.py
@@ -0,0 +1,20 @@
+
+'''
+place_of_customer = 'Hi {0}, are you in {1} '
+name = 'Harsha'
+place = 'Kanpur'
+print(place_of_customer.format(name,place))
+
+'''
+
+
+'''
+word_1 = "shine"
+word_2 = "Burn"
+author = "APJ kalam sir"
+quot = f"If you want to {word_1} like a sun, first {word_2} like a sun.\n {author}"
+print(type(quot))
+print(quot)
+'''
+
+
